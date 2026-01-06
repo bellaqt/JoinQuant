570da05fcebb9362df364f4d5fa0150be1200c66
bellaqt <chang20204@gmail.com>
2026-01-06 18:59:20 +0800

    222

--- JoinQuant.Server/Program.cs
+++ JoinQuant.Server/Program.cs
@@ -41,9 +41,13 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 //app.MapFallbackToFile("/index.html");
 
